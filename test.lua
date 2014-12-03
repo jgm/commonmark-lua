616fe5717026f0b903c220cf957f1ad66d673b94
John MacFarlane <jgm@berkeley.edu>
2014-12-03 10:12:27 -0800

    Let test.lua take an argument for the format to use.

--- test.lua
+++ test.lua
@@ -1,14 +1,20 @@
 #!/usr/bin/env luajit
 
 local cmark = require("commonmark")
-local html =require("commonmark.writer.html")
+
+local format = 'html'
+if arg[1] then
+   format = arg[1]
+end
+
+local W = require("commonmark.writer." .. format)
 
 local cur = doc
 
 local inp = io.read("*all")
 local doc = cmark.parse_document(inp, string.len(inp))
 
-writer = html.new()
+writer = W.new()
 io.write(writer.render(doc))
 
 for warning in writer.warnings() do
